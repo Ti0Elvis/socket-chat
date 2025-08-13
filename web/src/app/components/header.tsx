73798b84835f9047a818bf056e6111691fb6b837
Ti0Elvis <elvis.veramijares@gmail.com>
2025-08-13 04:17:14 +0200

    Toggle action on header component

--- web/src/app/components/header.tsx
+++ web/src/app/components/header.tsx
@@ -9,13 +9,11 @@ import {
 import { currentUser } from "@clerk/nextjs/server";
 import { ModeToggle } from "@/components/mode-toggle";
 import { SignedIn, SignOutButton } from "@clerk/nextjs";
-import { is_registered_current_user_on_api } from "../actions";
 import { MaxWidthWrapper } from "@/components/max-width-wrapper";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export async function Header() {
   const user = await currentUser();
-  const response = await is_registered_current_user_on_api();
 
   return (
     <header className="w-full h-16 sticky top-0 left-0 border-b bg-background/75 backdrop-blur-md">
@@ -32,34 +30,32 @@ export async function Header() {
           <span className="text-primary text-lg font-bold">socket-chat</span>
         </div>
         <div className="flex items-center gap-2">
-          {response.ok === true && (
-            <SignedIn>
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <Avatar className="cursor-pointer">
-                    <AvatarImage src={user?.imageUrl} alt="" />
-                    <AvatarFallback>
-                      {user?.firstName?.charAt(0)}
-                      {user?.lastName?.charAt(0)}
-                    </AvatarFallback>
-                  </Avatar>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent>
-                  <DropdownMenuItem asChild>
-                    <Link href="/profile">Profile</Link>
-                  </DropdownMenuItem>
-                  <DropdownMenuItem asChild>
-                    <Link href="/rooms">Rooms</Link>
-                  </DropdownMenuItem>
-                  <DropdownMenuItem asChild>
-                    <SignOutButton signOutOptions={{ redirectUrl: "/welcome" }}>
-                      <p className="w-full">Sign out</p>
-                    </SignOutButton>
-                  </DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </SignedIn>
-          )}
+          <SignedIn>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Avatar className="cursor-pointer">
+                  <AvatarImage src={user?.imageUrl} alt="" />
+                  <AvatarFallback>
+                    {user?.firstName?.charAt(0)}
+                    {user?.lastName?.charAt(0)}
+                  </AvatarFallback>
+                </Avatar>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuItem asChild>
+                  <Link href="/profile">Profile</Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem asChild>
+                  <Link href="/rooms">Rooms</Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem asChild>
+                  <SignOutButton signOutOptions={{ redirectUrl: "/welcome" }}>
+                    <p className="w-full">Sign out</p>
+                  </SignOutButton>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SignedIn>
           <ModeToggle />
         </div>
       </MaxWidthWrapper>
