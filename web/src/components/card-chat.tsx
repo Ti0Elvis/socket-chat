69c0d2f330cb6653a35e9b95c59502d9fbfc0e50
Ti0Elvis <elvis.veramijares@gmail.com>
2025-09-22 20:12:04 +0200

    Refactor message loading in CardChat and ShowMessages components to use react-query for improved data fetching and state management

--- web/src/components/card-chat.tsx
+++ web/src/components/card-chat.tsx
@@ -6,34 +6,12 @@ import { LogOutIcon } from "lucide-react";
 import { SendMessage } from "./send-message";
 import { Card, CardHeader } from "./ui/card";
 import { ShowMessages } from "./show-messages";
-import { useQuery } from "@tanstack/react-query";
-import { find_all_messages_by_room_id } from "@/app/(root)/messages/actions";
 
 interface Props {
   room: Room;
 }
 
 export function CardChat({ room }: Readonly<Props>) {
-  const query = useQuery({
-    queryKey: ["messages", room._id],
-    queryFn: async () => {
-      const response = await find_all_messages_by_room_id(room._id);
-
-      if (response.error) {
-        throw new Error(response.error.message);
-      }
-
-      return response.data;
-    },
-    refetchOnWindowFocus: false,
-    staleTime: Infinity,
-    enabled: !!room._id,
-  });
-
-  if (query.error !== null) {
-    throw new Error(query.error.message);
-  }
-
   return (
     <Card className="w-full h-full flex flex-col">
       <CardHeader className="border-b flex items-center justify-between">
@@ -44,12 +22,7 @@ export function CardChat({ room }: Readonly<Props>) {
           </Link>
         </Button>
       </CardHeader>
-      {query.isLoading === true && (
-        <div className="p-4">Loading messages...</div>
-      )}
-      {query.data !== undefined && (
-        <ShowMessages previous_messages={query.data} />
-      )}
+      <ShowMessages room_id={room._id} />
       <SendMessage room_id={room._id} />
     </Card>
   );
--- web/src/components/show-messages.tsx
+++ web/src/components/show-messages.tsx
@@ -4,16 +4,33 @@ import { useUser } from "@clerk/nextjs";
 import { Message } from "@/types/message";
 import { CardMessage } from "./card-message";
 import { ScrollArea } from "./ui/scroll-area";
-import { useContext, useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { useContext, useEffect, useRef, useState } from "react";
 import { MessageSocketContext } from "@/context/socket-message";
+import { find_all_messages_by_room_id } from "@/app/(root)/messages/actions";
 
 interface Props {
-  previous_messages: Array<Message>;
+  room_id: string;
 }
 
-export function ShowMessages({ previous_messages }: Readonly<Props>) {
+export function ShowMessages({ room_id }: Readonly<Props>) {
+  const ref = useRef<HTMLDivElement | null>(null);
   const context = useContext(MessageSocketContext)!;
-  const [messages, setMessages] = useState<Array<Message>>(previous_messages);
+  const [messages, setMessages] = useState<Array<Message>>([]);
+
+  const query = useQuery({
+    queryKey: ["messages", room_id],
+    queryFn: async () => {
+      const response = await find_all_messages_by_room_id(room_id);
+
+      if (response.error) {
+        throw new Error(response.error.message);
+      }
+
+      setMessages(response.data ?? []);
+      return response.data;
+    },
+  });
 
   const { user } = useUser();
 
@@ -27,19 +44,33 @@ export function ShowMessages({ previous_messages }: Readonly<Props>) {
     };
   }, [context.socket]);
 
+  useEffect(() => {
+    ref.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+
+  if (query.error !== null) {
+    throw new Error(query.error.message);
+  }
+
   return (
     <CardContent className="flex-1 overflow-hidden">
-      <ScrollArea className="h-full">
-        {messages.map((message) => {
-          return (
-            <CardMessage
-              key={message._id}
-              message={message}
-              user_id={user?.id}
-            />
-          );
-        })}
-      </ScrollArea>
+      {query.isLoading === true && (
+        <div className="p-4">Loading messages...</div>
+      )}
+      {query.data !== undefined && (
+        <ScrollArea className="h-full">
+          {messages.map((message) => {
+            return (
+              <CardMessage
+                key={message._id}
+                message={message}
+                user_id={user?.id}
+              />
+            );
+          })}
+          <div ref={ref} />
+        </ScrollArea>
+      )}
     </CardContent>
   );
 }
